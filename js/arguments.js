caa4ddba81745c787f49be75b84a7f88cf810f79
Shafiulislam <ishafiul900@gmail.com>
2023-02-28 18:11:20 +0600

    add complid

---
+++ js/arguments.js
@@ -0,0 +1,20 @@
+function sum(a, b, c, d){
+     const total = a + b + c + d;
+     return total;
+}
+
+const pass = sum(2, 4, 5, 6);
+// console.log(pass)
+
+
+
+let a = 8;
+let b = 2;
+function multiply(num1, num2){
+    num1 = 1;
+    const total = num1 * num2;
+   return total;
+}
+// console.log(outPut)
+const outPut = multiply(a, b)
+console.log(outPut)
