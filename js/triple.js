fdea9139083827f1038ae78bf7e99b5ac93d3da1
Shafiulislam <ishafiul900@gmail.com>
2023-02-28 13:24:54 +0600

    add typt.js

---
+++ js/triple.js
@@ -0,0 +1,18 @@
+const first = 2;
+const second = '2';
+// === ans There are not equal
+if(first === second){
+    console.log('There are equal')
+}
+else{
+    console.log("There are not equal")
+}
+
+
+// == ans there are equal
+if(first == second){
+    console.log('There are equal')
+}
+else{
+    console.log("There are not equal")
+}
